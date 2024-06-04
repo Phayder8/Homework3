a384eeb909027bd6c6c84b566fc89bed173cfd8d
Phayder8 <witcher2567@gmail.com>
2024-06-04 23:11:47 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,12 @@
+name = "Ilya"
+print(name, type(name))
+age = 28
+print(age, type(age))
+age = 28 + 1
+print(age, type(age))
+is_student = True
+print(is_student, type(is_student))
+print("Name: ", name)
+print("Age: ", age - 1)
+print("New Age: ",age)
+print("Is_student: ", is_student)
